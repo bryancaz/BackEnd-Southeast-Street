d1d1a2ad4e7924ba2b572b2a32f468902568311e
bryancaz <103477342+bryancaz@users.noreply.github.com>
2023-04-02 22:51:18 -0600

    Products and User routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+///RUTAS DE PRODUCTOS Y USUARIOS
 app.use( '/products', require('./products'));
 app.use('/auth', require('./auth'));
 
